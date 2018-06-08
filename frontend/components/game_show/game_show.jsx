fb1f9c9b132667aa4e957c2c86ac04aed9b8e328
Marshall <marshallycheng@gmail.com>
2018-06-08 15:00:29 -0700

    Finish preliminary game show styling

--- frontend/components/game_show/game_show.jsx
+++ frontend/components/game_show/game_show.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { homeIcon } from '../logo';
+import { sendIcon, homeIcon, favoriteIcon, fullStar, halfStar, emptyStar } from '../logo';
+import { Link } from 'react-router-dom';
 import NavBarContainer from '../nav/nav_bar_container';
 
 class GameShow extends React.Component {
@@ -19,23 +20,71 @@ class GameShow extends React.Component {
     } else {
     return (
       <div className="game-show-page">
+        <Link to="/" className="home-button">{homeIcon} Home</Link>
         <div className="game-show-content">
-          <div class="home-button">
-            {homeIcon} Home
-          </div>
+
+
           <div className="game-show-container">
             <div className="container-links">
+              <button className="send-button">{sendIcon} Send</button>
+              <button className="favorite-button">{favoriteIcon} Favorite</button>
             </div>
             <div className="container-content">
               <span className="game-show-title">
                 {game.title}
               </span>
               <img className="game-show-image" src={`${game.image_url}`}/>
-              <div className="review-form">
-                Review Form
+              <div className="game-details">
+                <div className="game-stats">
+                  <div className="game-stats-left">
+                    <span className="game-score">
+                      {fullStar}
+                      {fullStar}
+                      {fullStar}
+                      {halfStar}
+                      {emptyStar}
+                      3.5
+                    </span>
+                    <span className="game-release-date">
+                      <span className="game-release-date-label">
+                        Released on:
+                      </span>
+                      Jan 26, 1995
+                    </span>
+                  </div>
+                  <div className="game-stats-right">
+                    <span className="game-genres">
+                      <span className="game-genres-label">
+                        Genres:
+                      </span>
+                      Action
+                    </span>
+                    <span className="game-rating">
+                      <span className="game-rating-label">
+                        Rating:
+                      </span>
+                      T
+                    </span>
+                  </div>
+
+
+                </div>
+                <div className="review-form">
+                  Review form
+                </div>
               </div>
               <div className="game-show-description">
                 {game.description}
+              </div>
+            </div>
+            <div className="game-reviews">
+              <div className="review-header">
+                Reviews
+              </div>
+              <div className="reviews-list">
+                reviews list
+                reviews list
+                reviews list
               </div>
             </div>
           </div>
