92a01fd2751c068af486ed68a81c15d35f3e7048
Marshall <marshallycheng@gmail.com>
2018-06-14 10:07:37 -0700

    Finish basic user profile

--- frontend/components/user_profile/user_profile.jsx
+++ frontend/components/user_profile/user_profile.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import NavBarContainer from '../nav/nav_bar_container';
+import GameIndexItem from '../game_index/game_index_item';
+import { profileSendIcon, flagIcon } from '../logo';
 
 class UserProfile extends React.Component {
   constructor(props){
     super(props);
   }
 
+  componentDidMount(){
+    this.props.fetchGames();
+  }
+
+  randomSelection(num){
+    const randomGames = this.randomize(this.props.games);
+    return randomGames.slice(0,num);
+  }
+
+  randomize(array){
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
   render(){
-    const {currentUser} = this.props;
+    const { games, currentUser} = this.props;
     const reviewText = (currentUser.review_ids.length === 1) ? "review" : "reviews";
-    
     return (
       <div className="user-profile-page">
         <NavBarContainer />
         <div className="user-profile-content">
           <div className="user-profile-info">
             <div className="user-profile-buttons">
-              <button className="user-message-button">Message</button>
-              <button className="user-follow-button">Follow</button>
+              <div className="user-left-buttons">
+                  {profileSendIcon}
+                  {flagIcon}
+              </div>
+              <div className="user-right-buttons">
+                <button className="user-message-button">Message</button>
+                <button className="user-follow-button">Follow</button>
+              </div>
             </div>
             <div className="user-profile-middle">
               <div className="user-profile-middle-left">
@@ -31,17 +55,22 @@ class UserProfile extends React.Component {
               <div className="user-profile-middle-right">
                 <img
                   className="user-profile-image"
-                  src={`${currentUser.image_url}`}
+                  src={`https://thumbs.dreamstime.com/z/man-virtual-reality-cyber-play-video-game-wear-digital-glasses-profile-icon-flat-design-vector-illustration-69046697.jpg`}
                 />
               </div>
             </div>
           </div>
+          <div className="games-header">
+            Check out these games!
+          </div>
           <div className="user-profile-games">
+            {this.randomSelection(14).map(game => {
+              return <GameIndexItem key={game.id} game={game}/>;
+            })}
           </div>
         </div>
       </div>
     );
   }
 }
-
 export default UserProfile;
--- frontend/components/user_profile/user_profile_container.js
+++ frontend/components/user_profile/user_profile_container.js
@@ -1,15 +1,17 @@
 import { connect } from 'react-redux';
 import UserProfile from './user_profile';
+import { fetchGames } from '../../actions/game_actions';
 
 const msp = state => {
   return {
-    currentUser: state.entities.users[state.session.id]
+    currentUser: state.entities.users[state.session.id],
+    games: Object.values(state.entities.games)
   };
 };
 
 const mdp = dispatch => {
   return {
-
+    fetchGames: () => dispatch(fetchGames())
   };
 };
 
